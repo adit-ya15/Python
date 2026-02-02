4471b5fe8fb3e75ba239717e892823f91ec36149
adit-ya15 <aditya262701@gmail.com>
2026-02-02 23:46:59 +0530

    chapter 01 completed

---
+++ chapter-01/first.py
@@ -0,0 +1,13 @@
+message = """Hello, welcome to the Python's programming world!
+Now you going to become python 
+pro
+"""
+print(message)
+
+numbers = [1, 2, 3, 4, 5]
+total = 0;
+for number in numbers:
+    total += number 
+print("The total is:", total)
+#mudule : is a file containig code written by somebody else
+#pip : is the package manager for python
