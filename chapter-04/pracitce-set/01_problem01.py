6d5f0e27c9e11313bb601df41bc526116d082cd9
adit-ya15 <aditya262701@gmail.com>
2026-02-04 15:34:41 +0530

    Problem 01 of chapter 04 is done

---
+++ chapter-04/pracitce-set/01_problem01.py
@@ -0,0 +1,9 @@
+dict = {
+    "Aadmi" : "Man",
+    "Ladki" : "Girl",
+    "Kursi" : "Chair",
+    "Paani" : "Water"
+}
+
+word = input("Enter the word whose english you don't know: ")
+print(dict[word])
