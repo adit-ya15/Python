404b2830414fbe56d41d6b9fd3eb94a878ae5f57
adit-ya15 <aditya262701@gmail.com>
2026-02-04 17:42:31 +0530

    Problem 01 of chapter 05 is done

---
+++ chapter-05/pracitce-set/01_problem01.py
@@ -0,0 +1,15 @@
+def greatestOfThree():
+
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
+    c = int(input("Enter third number : "))
+
+    if(a > b and a > c):
+        print("Greatest number is :",a)
+    elif(b > c and b > a):
+        print("Greatest number is :",b)
+    else:
+        print("Greatest number is :",c)    
+# end def
+
+greatestOfThree()
