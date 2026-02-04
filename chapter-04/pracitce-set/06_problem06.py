030550dca1bea2e8782afddc4f246881975833f4
adit-ya15 <aditya262701@gmail.com>
2026-02-04 16:01:33 +0530

    Problem 06 of chapter 04 done

---
+++ chapter-04/pracitce-set/06_problem06.py
@@ -0,0 +1,19 @@
+d = {}
+
+name = input("Enter the name of your friend: ")
+lang = input("Enter their lang: ")
+d.update({name:lang})
+
+name = input("Enter the name of your friend: ")
+lang = input("Enter their lang: ")
+d.update({name:lang})
+
+name = input("Enter the name of your friend: ")
+lang = input("Enter their lang: ")
+d.update({name:lang})
+
+name = input("Enter the name of your friend: ")
+lang = input("Enter their lang: ")
+d.update({name:lang})
+
+print(d)
