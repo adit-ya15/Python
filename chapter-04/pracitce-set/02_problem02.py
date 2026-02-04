33b363c36faed070aabd035c5cf09b52b00b1ea5
adit-ya15 <aditya262701@gmail.com>
2026-02-04 15:42:42 +0530

    Problem 02 of chapter 04 done

---
+++ chapter-04/pracitce-set/02_problem02.py
@@ -0,0 +1,27 @@
+once = set()
+
+s1 = int(input("Enter the first number: "))
+once.add(s1)
+
+s2 = int(input("Enter the second number: "))
+once.add(s2)
+
+s3 = int(input("Enter the thired number: "))
+once.add(s3)
+
+s4 = int(input("Enter the fourth number: "))
+once.add(s4)
+
+s5 = int(input("Enter the fifth number: "))
+once.add(s5)
+
+s6 = int(input("Enter the sixth number: "))
+once.add(s6)
+
+s7 = int(input("Enter the seventh number: "))
+once.add(s7)
+
+s8 = int(input("Enter the eight number: "))
+once.add(s8)
+
+print(once)
