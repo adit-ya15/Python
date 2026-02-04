4abcbdb805ae6509f689dc1026bba82e6ced617a
adit-ya15 <aditya262701@gmail.com>
2026-02-04 16:06:05 +0530

    Problem 07 of chapter 04 done

---
+++ chapter-04/pracitce-set/07_problem.py
@@ -0,0 +1,14 @@
+d = {}
+
+name = input("Enter the name of your friend: ")
+lang = input("Enter their lang: ")
+d.update({name:lang})
+
+name = input("Enter the name of your friend: ")
+lang = input("Enter their lang: ")
+d.update({name:lang})
+
+#if the lang of two friends is same then nothing will happen all thing work fine
+
+print(len(d))
+print(d)
