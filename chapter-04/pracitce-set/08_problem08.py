be070e4f8176590c123bfe35cb6d523432d68164
adit-ya15 <aditya262701@gmail.com>
2026-02-04 16:10:22 +0530

    Problem 08 done of chapter 04

---
+++ chapter-04/pracitce-set/08_problem08.py
@@ -0,0 +1,5 @@
+s = {8, 7, 12, "Harry", [1,2]}
+
+# We cannot add list in a set because list is mutable and unhashable and then question can we chnge the list in element in
+# the set become pointless
+print(s)
