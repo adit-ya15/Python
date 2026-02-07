e806dda9293b0f66c499eab424be248e24ed230b
adit-ya15 <aditya262701@gmail.com>
2026-02-07 18:00:29 +0530

    Problem  10 of chapter 06 is done

---
+++ chapter-06/practice-set/problem10/10_problem.py
@@ -0,0 +1,7 @@
+#This code is logically work but the line f.wrrite() is unncessary 
+# with open("file.txt","w") as f:
+#     f.write("")
+
+#correct version
+with open("file.txt","w") as f:
+    pass
