ceebcb405145b8b618e70b032464e2e114701235
adit-ya15 <aditya262701@gmail.com>
2026-02-08 15:42:19 +0530

    Problem -02 of chapter -08 is done

---
+++ chapter-08/practice-set/problem-02/02_problem.py
@@ -0,0 +1,7 @@
+name = input("Enter the name of the : ")
+marks = int(input("Enter the marks of the student : "))
+conatact_no = int(input("Enter the contact number of the student : "))
+
+ans = "The name of the student is {}, his marks are {} and phone number is {}".format(name,marks,conatact_no)
+
+print(ans)
